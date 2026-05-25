e9cf8ba390d379e720d5118d5b86de1fd475efab
Puryll <YllPllana11@outlook.com>
2026-05-25 17:13:59 +0200

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import '../components/Navbar.css';
+import logo from '../assets/logo.png';
 
 
 function Navbar() {
   return (
-    <nav className="navbar">
-
+    <div className="navbar-container">
+      <nav className="navbar">
       <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/Product">Product</Link></li>
@@ -14,9 +15,14 @@ function Navbar() {
         <li><Link to="/Contact">Contact</Link></li>
       </ul>
 
-      {/*<Link to="/LoginPage"><button className="login-button">Login</button></Link>*/}
-        
-    </nav>
+        {/*<Link to="/LoginPage"><button className="login-button">Login</button></Link>*/}
+          
+      </nav>
+      <div className="logo-container">
+      <h1>Hekuraria Driloni AD </h1>
+      <img className="logo" src={logo} alt="Logo" />
+      </div>
+    </div>
   );
 }
 
