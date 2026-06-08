12e3e56fb506dfa5cb6da500c205800da6a40c67
Puryll <YllPllana11@outlook.com>
2026-06-08 16:59:00 +0200

    update

--- src/Product.js
+++ src/Product.js
@@ -13,11 +13,11 @@ import foto11 from "./assets/foto11.png";
 const products = [
     {id:1, name:"Sprej ngjyra rezistente ndaj temperaturave të larta", description:"Përmban 300ml, rezistente deri në 700°C, prodhim Gjerman", img:foto1, saleprice:19.99},
     {id:2, name:"Sprej ngjyra ,nuanca të ndryshme", description:"Përmban 300ml, bojë akrilike premium, prodhim Gjerman", img:foto3, saleprice:9.99},
-    {id:3, name:"Sprej ngjyra ,nuanca të ndryshme", description:"Përmban 150ml, paket e vogël, prodhim Gjerman", img:foto4, saleprice:14.99},
+    {id:3, name:"Sprej ngjyra ,nuanca të ndryshme", description:"Përmban 150ml, paket e vogël, prodhim Gjerman", img:foto4, saleprice:9.99},
     {id:4, name:"Sprej ngjyra për dru,nuanca të ndryshme", description:"Përmban 300ml, prodhim për dru, prodhim Gjerman", img:foto5, saleprice:14.99},
     {id:5, name:"Sprej-llak efekt", description:"Përmban 300ml, efekt të ngjyrës Gold-Chrome, prodhim Gjerman", img:foto6, saleprice:9.99},
-    {id:6, name:"Sprej për mbrojtjëse të veturave", description:"Përmban 300ml, mbrojtjëse të veturave, prodhim Gjerman", img:foto7, saleprice:24.99},
-    {id:7, name:"Kit (stuko) për vetura-Sprej", description:"Përmban 300ml, kit i plotë për vetura, prodhim Gjerman", img:foto8, saleprice:29.99},
+    {id:6, name:"Sprej për mbrojtjëse të veturave", description:"Përmban 300ml, mbrojtjëse të veturave, prodhim Gjerman", img:foto7, saleprice:14.99},
+    {id:7, name:"Kit (stuko) për vetura-Sprej", description:"Përmban 300ml, kit i plotë për vetura, prodhim Gjerman", img:foto8, saleprice:24.99},
     {id:8, name:"Sprej për dekurime", description:"Përmban 300ml, prodhim Gold-Silver-Ice crystals, prodhim Gjerman", img:foto9, saleprice:14.99},
     {id:9, name:"Shkumë kundër zjarrit", description:"Përmban 500ml, prodhim Gjerman", img:foto10, saleprice:2.49},
     {id:10, name:"Shkumë pa presion, për pompë", description:"Përmban 500ml, prodhim Gjerman", img:foto11, saleprice:2.49}
