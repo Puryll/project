fdd703224e77febec4134e308830d3c6cffd7646
Puryll <YllPllana11@outlook.com>
2026-06-08 17:07:53 +0200

    update

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -11,10 +11,10 @@ function Footer () {
                     <a href="instagram.com" className="a2">isntagram</a>
                 </ul>
                 <div className="MadeFrom">
-                    <p>U bë nga</p>
+                    <p>U krijua nga</p>
                     <h2>Yll Pllana</h2>
                     <h3>Vigjilent Zeqiri</h3>
-                    <h4>Diar Peci</h4>
+                    <h4>Dijar Peci</h4>
                 </div>
             </div>
             <p className="copy">&copy; 2026 Hekuraria Driloni AD. All rights reserved.</p>
