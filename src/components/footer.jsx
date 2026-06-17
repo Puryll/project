d39a2d61b15087eaf7c4330f5601764d7fb14aa1
Puryll <YllPllana11@outlook.com>
2026-06-17 18:41:11 +0200

    update

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -10,6 +10,10 @@ function Footer () {
                     <a href="https://www.facebook.com/driloniad" className="a1">facebook</a>
                     <a href="instagram.com" className="a2">instagram</a>
                 </ul>
+                <div className="info-item">
+                    <a href="tel:+355691234567">+383 044 307 925</a><br />
+                    <a href="tel:+355422345678">+383 049 551 704</a>
+                </div>
                 <div className="MadeFrom">
                     <p>U krijua nga</p>
                     <h2>Yll Pllana</h2>
